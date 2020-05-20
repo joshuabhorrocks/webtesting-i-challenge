516fdb1796de6adb18905baf459ba4f467a7d939
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-05-20 14:51:04 -0600

    Tested MVP and completed get Stretch Goal

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,45 @@ module.exports = {
   get,
 };
 
+// item = {
+//   name: "",
+//   durability: int,
+//   enhancement: int
+// }
+
 function succeed(item) {
-  return { ...item };
+  let enhancement = item.enhancement;
+
+  if (enhancement < 20) {
+    return enhancement + 1;
+  } else {
+    return enhancement;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  let enhancement = item.enhancement;
+  let durability = item.durability;
+
+  if (enhancement < 15) {
+    return durability - 5;
+  } else if (enhancement >= 15) {
+    return durability - 10;
+  } else if (enchantment > 16) {
+    return enhancement - 1;
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  let durability = item.durability;
+  return durability = 100
 }
 
 function get(item) {
-  return { ...item };
+  let enhancement = item.enhancement
+  if (enhancement > 0) {
+    return `[+${enhancement}] ${item.name}`;
+  } else {
+    return item;
+  }
 }
